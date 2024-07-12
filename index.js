004808acb955ce00aac64481eba1a55ddabfc41d
ammar arief <ammararief321@gmail.com>
2024-07-12 08:52:04 +0700

    hide html

--- index.js
+++ index.js
@@ -75,11 +75,12 @@ const startSock = async () => {
 
 startSock();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.get('/auth-status', async (req, res) => {
     res.status(200).json({ authenticated: isAuthenticated });
